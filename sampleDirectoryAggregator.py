aa4b714359511118b6a7311bac9df0b488c24bda
Avneesh Sarwate <avneeshsarwate@gmail.com>
2017-09-13 15:13:05 -0400

    possibly a better way to do it - generate a file of all paths without moving stuff around (could be faster or used on remote directories)

--- sampleDirectoryAggregator.py
+++ sampleDirectoryAggregator.py
@@ -4,8 +4,18 @@ import shutil
 
 targetDir = "./target"
 
+fileNames = []
+
 for directory in os.walk("./sampDir0"):
 	for file in directory[2]:
-		print directory[0]+"/"+file, targetDir+"/"+file
-		shutil.copyfile(directory[0]+"/"+file, targetDir+"/"+file)
+		oldPath = directory[0]+"/"+file
+		newPath = targetDir+"/"+file
+		print oldPath, newPath
+		#shutil.copyfile(oldPath, newPath)
+		fileNames.append(oldPath)
+
+allPaths = open("allPaths.txt", 'w')
+allPaths.write("\n".join(fileNames))
+allPaths.write("\n")
+
 
